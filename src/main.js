04ff051776f8ab5876837a51cee5640ee7706efb
сonfident_computer_user <btf9458@gmail.com>
2025-02-19 17:20:34 +0500

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,27 @@
+
+import Swiper from "swiper";
+
+
+
+const swiper = new Swiper('.swiper', {
+  simulateTouch: false,
+  speed: 5, 
+  loop: true, 
+});
+
+const buttons = document.querySelectorAll('.swiper__button');
+let timeout;
+
+buttons.forEach((button) => {
+  button.addEventListener('mouseenter', () => {
+    const slideIndex = parseInt(button.getAttribute('data-slide'));
+    timeout = setTimeout(() => {
+      swiper.slideTo(slideIndex);
+    }, 50); 
+  });
+
+  button.addEventListener('mouseleave', () => {
+    clearTimeout(timeout);
+  });
+});
+
